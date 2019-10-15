d0c6465863580f83297a6974035a7afc8cd2f899
Arfon Smith <arfon.smith@gmail.com>
2019-10-15 13:45:04 +0100

    Run the local generate

--- generate.rb
+++ generate.rb
@@ -1,6 +1,7 @@
-require 'octokit'
-
-puts Octokit::VERSION
+require 'bolognese'
 
 puts "The repository is #{ENV['GITHUB_REPOSITORY']}"
 puts "The commit is #{ENV['GITHUB_SHA']}"
+
+# Detect a file
+# Run bolognese
