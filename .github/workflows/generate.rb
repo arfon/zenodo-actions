22e58ac79aa794e995abaf8af5a88f16c2c2d1b7
Arfon Smith <arfon.smith@gmail.com>
2019-10-15 14:36:01 +0100

    Test run

--- .github/workflows/generate.rb
+++ .github/workflows/generate.rb
@@ -3,5 +3,12 @@ require 'bolognese'
 puts "The repository is #{ENV['GITHUB_REPOSITORY']}"
 puts "The commit is #{ENV['GITHUB_SHA']}"
 
-# Detect a file
-# Run bolognese
+files_to_detect = [
+  "filename"
+]
+
+# See what files exist at the repository root
+detected = files_to_detect.select { |file| File.exists?(file) }
+
+# Fail the script if none of the desired files are detected
+fail unless detected.any?
--- .github/workflows/open_pull_request.rb
+++ .github/workflows/open_pull_request.rb
@@ -1,1 +1,5 @@
 puts "Hello from pull request generator"
+
+# Make a branch
+# Commit codemeta.json
+# Open a pull request
